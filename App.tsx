c596650f988d019730ef74d80fe91e889cd14007
Casso Station4 <casso.station4@gmail.com>
2024-11-14 16:46:00 +0700

    maxFontSizeMultiplier stopped working

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+/**
+ * Below code is to limit the max size of text font user can custom in Accessibility
+ * StackOverflow: https://stackoverflow.com/a/65193181/27724785
+ */
+// interface TextWithDefaultProps extends Text {
+//   defaultProps?: { maxFontSizeMultiplier?: number; allowFontScaling?: boolean };
+// }
+// interface TextInputWithDefaultProps extends TextInput {
+//   defaultProps?: { maxFontSizeMultiplier?: number; allowFontScaling?: boolean };
+// }
+// (Text as unknown as TextWithDefaultProps).defaultProps =
+//   (Text as unknown as TextWithDefaultProps).defaultProps || {};
+// (
+//   Text as unknown as TextWithDefaultProps
+// ).defaultProps!.maxFontSizeMultiplier = 1.1;
+// (Text as unknown as TextWithDefaultProps).defaultProps!.allowFontScaling = true;
+
+// (TextInput as unknown as TextInputWithDefaultProps).defaultProps =
+//   (TextInput as unknown as TextInputWithDefaultProps).defaultProps || {};
+// (
+//   TextInput as unknown as TextInputWithDefaultProps
+// ).defaultProps!.maxFontSizeMultiplier = 1.1;
+// (
+//   TextInput as unknown as TextInputWithDefaultProps
+// ).defaultProps!.allowFontScaling = true;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text allowFontScaling={true} maxFontSizeMultiplier={1.2}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
